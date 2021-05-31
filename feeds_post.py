5d1b18077c8affc426fe9695ff0d06fe6e157616
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2021-05-31 16:30:06 +0200

    New version of the feed

--- feeds_post.py
+++ feeds_post.py
@@ -1,7 +1,13 @@
 from pyshorteners import Shortener
 import feedparser
-from pprint import pprint
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+API_KEY = "8d3dc08c8a0d27a88e41a46b14b8c9106fc939c4"
+API_USER = "cavatrendy"
+
+s = Shortener(api_key=API_KEY)
+
 # food_url = "http://news.google.com/news?q=food+blockchain&hl=en-US&sort=date&gl=US&num=100&output=rss"
 # agriculture_url = "http://news.google.com/news?q=agriculture+blockchain&hl=en-US&sort=date&gl=US&num=100&output=rss"
 
@@ -14,7 +20,7 @@ class ParseFeed():
         '''
         Get the text from html and do some cleaning
         '''
-        soup = BeautifulSoup(html)
+        soup = BeautifulSoup(html, features="lxml")
         text = soup.get_text()
         text = text.replace('\xa0', ' ')
         return text
@@ -24,33 +30,32 @@ class ParseFeed():
         Parse the URL, and print all the details of the news
         '''
         feeds = feedparser.parse(self.feed_url).entries
-        feders = {"Title": [], "Description": [], "Url": [], "PubDate": []}
+        dictionary = {"TITLE": [], "URL": [], "DESCR": [], "PUBDATE": []}
         for f in feeds:
-            pprint({
-                'Description': self.clean(f.get("description", "")),
-                'Published Date': f.get("published", ""),
-                'Title': f.get("title", ""),
-                'Url': f.get("link", "")
-            })
+            dictionary["TITLE"].append(f.get("title", ""))
+            dictionary["URL"].append(f.get("link", ""))
+            dictionary["DESCR"].append(self.clean(f.get("description", "")))
+            dictionary["PUBDATE"].append(f.get("published", ""))
+        new_data = [{"TITLE": s, "URL": t, "DESCR": e, "PUBDATE": l} for s, t, e, l in
+                    zip(dictionary["TITLE"], dictionary["URL"], dictionary["DESCR"], dictionary["PUBDATE"])]
+        return new_data
 
 
-def url_parsing():
-    url_list = ["http://news.google.com/news?q=food+blockchain&hl=en-US&sort=date&gl=US&num=100&output=rss",
-                "http://news.google.com/news?q=agriculture+blockchain&hl=en-US&sort=date&gl=US&num=100&output=rss"]
-    for link in url_list:
-        feed = ParseFeed(link)
-        link_dictionary = feed.parse().__dict__
-    return link_dictionary
+feed = ParseFeed("http://news.google.com/news?q=food+blockchain&hl=en-US&sort=date&gl=US&num=100&output=rss")
+link_dictionary = feed.parse()
+date_obj = datetime.today().strftime("%a, %d %b %Y %H:%M:%Scapi")
+##'Fri, 07 May 2021 07:00:00 GMT')
+
+print(date_obj)
+
+for data in link_dictionary:
+    for a in data.items():
+        print(a)
 
 
-print(url_parsing())
 
-# for t in feed.parse():
-#     print (t)
-API_KEY = "8d3dc08c8a0d27a88e41a46b14b8c9106fc939c4"
-API_USER = "cavatrendy"
 
-s = Shortener(api_key=API_KEY)
+
 
 # for l in df_diff.get("link"):
 #     try:
