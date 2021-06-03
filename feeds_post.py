d015d5ca1d4e2a2e2a4bacde5d8e6ad95f00d18b
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2021-06-03 19:02:38 +0200

    New version of the feed

--- feeds_post.py
+++ feeds_post.py
@@ -1,16 +1,9 @@
-from pyshorteners import Shortener
 import feedparser
 from bs4 import BeautifulSoup
 from datetime import datetime
 import pytz
+from operator import itemgetter
 
-API_KEY = "8d3dc08c8a0d27a88e41a46b14b8c9106fc939c4"
-API_USER = "cavatrendy"
-
-s = Shortener(api_key=API_KEY)
-
-# food_url = "http://news.google.com/news?q=food+blockchain&hl=en-US&sort=date&gl=US&num=100&output=rss"
-# agriculture_url = "http://news.google.com/news?q=agriculture+blockchain&hl=en-US&sort=date&gl=US&num=100&output=rss"
 
 class ParseFeed():
 
@@ -41,60 +34,37 @@ class ParseFeed():
                     zip(dictionary["TITLE"], dictionary["URL"], dictionary["DESCR"], dictionary["PUBDATE"])]
         return new_data
 
-
-feed = ParseFeed("http://news.google.com/news?q=food+blockchain&hl=en-US&sort=date&gl=US&num=100&output=rss")
-link_dictionary = feed.parse()
-
-
-
-for data in link_dictionary:
-    ###imposto la timezone gmt
-    tz_GMT = pytz.timezone('GMT')
-    date_obj = datetime.now(tz_GMT).strftime("%a, %d %b %Y %H:%M:%S")
-    ##pulisco la data di input
-    strip_gmt = data["PUBDATE"].rstrip(" GMT")
-    # setto uguale le date
-    data_pub = datetime.strptime(strip_gmt, "%a, %d %b %Y %H:%M:%S")
-    new_date = datetime.strptime(date_obj, "%a, %d %b %Y %H:%M:%S")
-    # creo il dizionario per i dati
-    new_dictionary = {"TITLE": [], "URL": [], "DESCR": [], "PUBDATE": []}
-    # setto la paramatrizzione
-    # print((new_date - data_pub).days)
-    print((data_pub - new_date ).days)
-    if (data_pub - new_date ).days < 7 :
-        print("superiorie " + str(data_pub))
+def cleaning_link(dictionary):
+    new_dictionary = {"TITLE": [], "URL": [],  "PUBDATE": []}
+    for data in dictionary:
+        # ###imposto la timezone gmt
+        # tz_GMT = pytz.timezone('GMT')
+        # date_obj = datetime.now(tz_GMT).strftime("%a, %d %b %Y %H:%M:%S")
+        # ##pulisco la data di input
+        # strip_gmt = data["PUBDATE"].rstrip(" GMT")
+        # # setto uguale le date
+        # data_pub = datetime.strptime(strip_gmt, "%a, %d %b %Y %H:%M:%S")
+        # new_date = datetime.strptime(date_obj, "%a, %d %b %Y %H:%M:%S")
+        # if (new_date - data_pub).days <= 1 :
+            # print("superiorie " + str(data_pub))
         new_dictionary["TITLE"].append(data["TITLE"])
         new_dictionary["URL"].append(data["URL"])
-        new_dictionary["DESCR"].append(data["DESCR"])
+        # new_dictionary["DESCR"].append(data["DESCR"])
         new_dictionary["PUBDATE"].append(data["PUBDATE"])
 
-    else:
-        print("inferiore")
-
-publish_data = [{"TITLE": s, "URL": t, "DESCR": e, "PUBDATE": l} for s, t, e, l in
-            zip(new_dictionary["TITLE"], new_dictionary["URL"], new_dictionary["DESCR"], new_dictionary["PUBDATE"])]
-
-print(publish_data)
+    publish_data = sorted([{"TITLE": s, "URL": t, "PUBDATE": l} for s, t, l in
+                           zip(new_dictionary["TITLE"], new_dictionary["URL"], new_dictionary["PUBDATE"])],
+                          key=itemgetter('PUBDATE'), reverse=True)
+    return publish_data
 
 
+# feed_food = ParseFeed("http://news.google.com/news?q=food+blockchain&hl=en-US&sort=date&gl=US&num=100&output=rss")
+feed_food = ParseFeed("https://news.google.com/rss/search?q=food+blockchain+agriculture+blockchain+when:1d&hl=en-US&gl=US&ceid=US:en")
+# feed_agriculture = ParseFeed("http://news.google.com/news?q=agriculture+blockchain&hl=en-US&sort=date&gl=US&num=100&output=rss")
+link_food= feed_food.parse()
+# link_agri= feed_agriculture.parse()
 
-
-# for l in df_diff.get("link"):
-#     try:
-#         link_to_short.append(s.bitly.short(l))
-#         print(l)
-#     except:
-#         print("Not added " + l)
-#
-# title_to_short = []
-# for t in  df_diff.get("title"):
-#     title_to_short.append(t)
-#
-# def twitter_message (title, url):
-#     print("Creating message for" + title + url)
-#     create_mesage = f"{title} at {url}"
-#     return create_mesage
-#
-# for title, link in zip(title_to_short,link_to_short):
-#     a = twitter_message(title, link)
-#     print (a)
+food = cleaning_link(link_food)
+# agri = cleaning_link(link_agri)
+print(food)
+# print(agri)
--- post_tweet.py
+++ post_tweet.py
@@ -1,14 +1,37 @@
 from config import create_api
 import logging
-from feeds_post import df_diff
+from pyshorteners import Shortener
+from feeds_post import food
 logging.basicConfig(
     filename='twitter-bot.log', encoding='utf-8', level=logging.DEBUG)
 logger = logging.getLogger()
 
+API_KEY = "8d3dc08c8a0d27a88e41a46b14b8c9106fc939c4"
+API_USER = "cavatrendy"
+
+s = Shortener(api_key=API_KEY)
+def shorten_link(link):
+    short_link = s.bitly.short(link)
+    return short_link
+def twitter_message(title, url):
+    hashtag = "#foodtech #agritech #blockchain #innovation"
+    print("Creating message for " + title + url + " " + hashtag)
+    create_mesage = f"{title} at {url} {hashtag}"
+    if len(create_mesage) >= 281:
+        print("to loong" + len(create_mesage))
+    else:
+        create_mesage
+    return create_mesage
+
+# for pdata in publish_data:
+#     print(len(twitter_message(pdata["TITLE"], shorten_link(pdata["URL"]))))
+#     print(twitter_message(pdata["TITLE"], shorten_link(pdata["URL"])))
 
 def post_tweet(event="", context=""):
     api = create_api()
-    article = df_diff
+    for pdata in food:
+        article = twitter_message(pdata["TITLE"], shorten_link(pdata["URL"]))
+    # article = df_diff
     tweet = article.write_tweet()
     try:
         api.update_status(status=tweet)
