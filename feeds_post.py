7c2c1c03ab878b767e4c40c1b7b07ea89446f5c0
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2021-06-04 16:49:38 +0200

    New version of the feed with classes

--- feeds_post.py
+++ feeds_post.py
@@ -82,8 +82,6 @@ class TweetPreparion(ParseFeed):
         for i in self.cleaning_link():
             for j in students:
                 if j in i["TITLE"]:
-                    # print(i["TITLE"])
-                    # print(j, i)
                     dict_input["TITLE"].append(i["TITLE"])
                     dict_input["URL"].append(i["URL"])
                     dict_input["PUBDATE"].append(i["PUBDATE"])
@@ -97,61 +95,19 @@ class TweetPreparion(ParseFeed):
 feed_food = TweetPreparion("https://news.google.com/rss/search?q=food+blockchain+agriculture+blockchain+when:1d&hl=en-US&gl=US&ceid=US:en")
 link_food = feed_food.creating_data()
 print(link_food)
-# def cleaning_link(dictionary):
-#     new_dictionary = {"TITLE": [], "URL": [], "PUBDATE": []}
-#
-#     for data in dictionary:
-#         # ###imposto la timezone gmt
-#         # tz_GMT = pytz.timezone('GMT')
-#         # date_obj = datetime.now(tz_GMT).strftime("%a, %d %b %Y %H:%M:%S")
-#         # ##pulisco la data di input
-#         # strip_gmt = data["PUBDATE"].rstrip(" GMT")
-#         # # setto uguale le date
-#         # data_pub = datetime.strptime(strip_gmt, "%a, %d %b %Y %H:%M:%S")
-#         # new_date = datetime.strptime(date_obj, "%a, %d %b %Y %H:%M:%S")
-#         # if (new_date - data_pub).days <= 1 :
-#         # print("superiorie " + str(data_pub))
-#         # students = ('Agric', 'Food', 'AGRIC', 'FOOD', 'agric', 'food')
-#         # for words in data:
-#         #     if all(words in new_dictionary["TITLE"] for words in students):
-#         #
-#         new_dictionary["TITLE"].append(data["TITLE"])
-#         new_dictionary["URL"].append(data["URL"])
-#         # new_dictionary["DESCR"].append(data["DESCR"])
-#         new_dictionary["PUBDATE"].append(data["PUBDATE"])
-#
-#     publish_data = sorted([{"TITLE": s, "URL": t, "PUBDATE": l} for s, t, l in
-#                            zip(new_dictionary["TITLE"], new_dictionary["URL"], new_dictionary["PUBDATE"])],
-#                           key=itemgetter('PUBDATE'), reverse=True)
-#     return publish_data
-#
-#
-# def twitter_message(title, url):
-#     hashtag = "#foodtech #agritech #blockchain #innovation"
-#     print("Creating message for " + title + url + " " + hashtag)
-#     create_mesage = f"{title} at {s.bitly.short(url)} {hashtag}"
-#     if len(create_mesage) >= 281:
-#         print("to loong" + len(create_mesage))
-#     else:
-#         create_mesage
-#     return create_mesage
-#
-#
-# feed_food = ParseFeed(
-#     "https://news.google.com/rss/search?q=food+blockchain+agriculture+blockchain+when:1d&hl=en-US&gl=US&ceid=US:en")
-# link_food = feed_food.parse()
-# food = cleaning_link(link_food)
-# students = ['Agric', 'Agriculture', 'Food', 'AGRIC', 'AGRICULTURE', 'FOOD', 'agric', 'agricultre', 'food']
-# new_dictionary = {"TITLE": [], "URL": [], "PUBDATE": []}
-# for i in food:
-#     for j in students:
-#         if j in i["TITLE"]:
-#             # print(i["TITLE"])
-#             # print(j, i)
-#             new_dictionary["TITLE"].append(i["TITLE"])
-#             new_dictionary["URL"].append(i["URL"])
-#             new_dictionary["PUBDATE"].append(i["PUBDATE"])
-#
-# publish_data = sorted([{"TITLE": s, "URL": t, "PUBDATE": l} for s, t, l in
-#                        zip(new_dictionary["TITLE"], new_dictionary["URL"], new_dictionary["PUBDATE"])],
-#                       key=itemgetter('PUBDATE'), reverse=True)
+
+def twitter_message(title, url):
+    hashtag = "#foodtech #agritech #blockchain #innovation"
+    print("Creating message for " + title + url + " " + hashtag)
+    create_mesage = f"{title} at {s.bitly.short(url)} {hashtag}"
+    if len(create_mesage) >= 281:
+        print("to loong" + len(create_mesage))
+    else:
+        create_mesage
+    return create_mesage
+
+for a in link_food:
+    print(a)
+    print(a["TITLE"])
+
+
