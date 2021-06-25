0af3e68b3c992a44eeaf65f249046d919b60952b
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2021-06-25 12:09:12 +0200

    evn variable aggiunter

--- tweet_post.py
+++ tweet_post.py
@@ -17,91 +17,36 @@ api = tweepy.API(auth)
 post = main_post()
 len_post = len(post)
 
-# def twitter_check():
-#     twitter_title = {"TITLE": []}
-#     for status in tweepy.Cursor(api.user_timeline, screen_name='@CavaTrendy', tweet_mode="extended").items(
-#             len_post + 1):
-#         title_redux = status.full_text
-#         print(
-#             title_redux
-#         )
-#         twitter_title["TITLE"].append(title_redux)
-#     print(twitter_title)
-#     # twitter_title, post = [sorted(l, key=itemgetter('PUBDATE'))
-#     #                        for l in (twitter_title, post)]
-#     # pairs = zip(twitter_title, post)
-#     # difference = any(x != y for x, y in pairs)
-#     intersec = [item for item in post if item in twitter_title]
-#     sym_diff = [item for item in itertools.chain(post, twitter_title) if item not in intersec]
-#     if intersec:
-#         print(post)
-#     elif sym_diff:
-#         print(twitter_title)
-#     else:
-#         print("nulla")
+def twitter_check():
+    twitter_title = {"TITLE": []}
+    for status in tweepy.Cursor(api.user_timeline, screen_name='@CavaTrendy', tweet_mode="extended").items(
+            len_post + 1):
+        title_redux = status.full_text
+        twitter_title["TITLE"].append(title_redux)
+    for message in post:
+        # print(message["TITLE"])
+        if message["TITLE"] in twitter_title["TITLE"]:
+            print("in")
+        else:
+            new_message = message["TITLE"]
+    return new_message
 
-# for message in post:
-#     print(message["TITLE"])
-#     if message["TITLE"] in twitter_title.items():
-#
-#         print("in")
-#     else:
-#         print("not")
-#         print(message["TITLE"])
-#         new_message = message["TITLE"]
-# return new_message
+def twitter_message(text):
+    return api.update_status(text)
 
-
-# def twitter_message(text):
-#     return api.update_status(text)
-#
-#
 # print(twitter_check())
 
-twitter_title = {"TITLE": []}
-for status in tweepy.Cursor(api.user_timeline, screen_name='@CavaTrendy', tweet_mode="extended").items(
-        len_post + 1):
-    title_redux = status.full_text
-    print(
-        title_redux
-    )
-    twitter_title["TITLE"].append(title_redux)
-print(twitter_title)
-# twitter_title, post = [sorted(l, key=itemgetter('PUBDATE'))
-#                        for l in (twitter_title, post)]
-# pairs = zip(twitter_title, post)
-# difference = any(x != y for x, y in pairs)
-for posts in post:
-    for a, b in zip(posts, twitter_title):
-        for key, value in posts.items():
-            print(key, value)
-            # if value in b["TITLE"]:
-            #     print(key, value, b["TITLE"])
 
-# intersec = [item for item in message if item in twitter_title]
-# sym_diff = [item for item in itertools.chain(message, twitter_title) if item not in intersec]
-#
-# if intersec:
-#     print("intesec")
-# elif sym_diff:
-#     print("sym_diff")
-# else:
-#     print("nulla")
+count = 0
+while count <= len_post:
+    try:
+        twitter_message(twitter_check())
+        count +=1
+        print(count)
+        time.sleep(1800)
+    except tweepy.error.TweepError:
+        print("error")
 
-# count = 0
-#
-# while count <= len_post:
-#     try:
-#         twitter_message(twitter_check())
-#         count +=1
-#
-#         print(count)
-#     except tweepy.error.TweepError:
-#         print("error")
-
-
-# tweet = client.user_timeline(id=self.client_id, count=1)[0]
-# print(tweet.text)
 
 # if __name__ == '__main__':
 #     post = main_post()
